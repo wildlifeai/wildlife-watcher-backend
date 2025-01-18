324ef19a126e3faae2c1a47eb49b26469a6e3d44
Miha Drofenik <miha.drofenik@visionect.com>
2025-01-18 13:09:35 +0100

    Workflow updates

---
+++ src/api/desk/routes/desk.ts
@@ -0,0 +1,7 @@
+/**
+ * desk router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::desk.desk');
---
+++ src/api/desk/services/desk.ts
@@ -0,0 +1,7 @@
+/**
+ * desk service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::desk.desk');

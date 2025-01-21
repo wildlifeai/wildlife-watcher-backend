448bd403202a7cc71559b1ff03c53ed1b3ed81d8
Miha Drofenik <miha.drofenik@visionect.com>
2025-01-21 15:08:07 +0100

    Changes

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,24 @@ import path from "path";
 
 export default ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
-
+  console.log("env", JSON.stringify(env));
+  console.log({
+    connectionString: env("DATABASE_URL"),
+    host: env("DATABASE_HOST", "localhost"),
+    port: env.int("DATABASE_PORT", 5432),
+    database: env("DATABASE_NAME", "strapi"),
+    user: env("DATABASE_USERNAME", "strapi"),
+    password: env("DATABASE_PASSWORD", "strapi"),
+    ssl: env.bool("DATABASE_SSL", false) && {
+      key: env("DATABASE_SSL_KEY", undefined),
+      cert: env("DATABASE_SSL_CERT", undefined),
+      ca: env("DATABASE_SSL_CA", undefined),
+      capath: env("DATABASE_SSL_CAPATH", undefined),
+      cipher: env("DATABASE_SSL_CIPHER", undefined),
+      rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
+    },
+    schema: env("DATABASE_SCHEMA", "public"),
+  });
   const connections = {
     mysql: {
       connection: {
